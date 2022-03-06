9a2e635c17b249226b56194287e4fae4a90cfcba
fahmirumagutawan <fahmigutawan@gmail.com>
2022-03-06 20:06:39 +0700

    menambahkan onboard activity

---
+++ app/src/main/java/com/example/equal/onboarding/OnboardingActivity.kt
@@ -0,0 +1,7 @@
+package com.example.equal.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.equal.R
+
+class OnboardingActivity:AppCompatActivity(R.layout.onboarding_activity) {
+}
